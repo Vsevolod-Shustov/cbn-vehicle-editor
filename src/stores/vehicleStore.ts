0ae42f5e18273bb82141a25b6d246dc885c30d95
Vsevolod Shustov <vsevolod.shustov@gmail.com>
2026-05-29 09:19:53 +0300

    rename vehicle tiles store to vehicle store

---
+++ src/stores/vehicleStore.ts
@@ -0,0 +1,26 @@
+import { ref } from 'vue'
+import { defineStore } from 'pinia'
+import { useVehiclePartsStore } from './vehiclePartsStore'
+
+type VehicleTile = {
+  id: string
+  parts: Map<string, string>
+}
+
+export const useVehicleStore = defineStore('vehicle', () => {
+  const vehicleTiles = ref<Map<string, VehicleTile>>(
+    new Map([
+      [
+        '0 0',
+        {
+          id: '0 0',
+          parts: new Map([['structure', 'frame_cross']]),
+        },
+      ],
+    ]),
+  )
+
+  return {
+    vehicleTiles,
+  }
+})
