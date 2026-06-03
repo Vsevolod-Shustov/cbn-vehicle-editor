0d92eebca3c510829d8c745e6d69cd9a373adacb
Vsevolod Shustov <vsevolod.shustov@gmail.com>
2026-06-03 17:35:49 +0300

    github pages config

--- vite.config.ts
+++ vite.config.ts
@@ -12,6 +12,7 @@ export default defineConfig({
       '@': fileURLToPath(new URL('./src', import.meta.url)),
     },
   },
+  base: process.env.BASE_PATH || '/cbn-vehicle-editor/', // e.g., '/your-repo-name/'
   build: {
     outDir: 'docs',
     emptyOutDir: true,
